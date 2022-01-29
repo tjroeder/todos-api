065b933435f3e407e5a9137693c9a62c98df7b8f
Tim Roeder <timothyjroeder@gmail.com>
2022-01-28 17:33:56 -0700

    Feat: create user association and relationship tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'associations test' do
+    it { should have_many(:todos) }
+  end
+
+  describe 'validations tests' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+  end
+end
